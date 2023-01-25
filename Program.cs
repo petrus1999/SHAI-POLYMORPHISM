d3e089c0f0a2dd749a536e121dcd59a427ebb809
petrus <pshai671@gmail.com>
2023-01-25 12:00:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using SHAI_POLYMORPHISM;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal myAnimal = new Animal();  
+        Animal myPig = new Pig();  
+        Animal myDog = new Dog();
+
+        myAnimal.animalSound();
+        myPig.animalSound();
+        myDog.animalSound();
+    }
+
+}
